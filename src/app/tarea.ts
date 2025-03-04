5e51e7a30442adfef44eca994ded2e4073af8151
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2025-03-04 14:03:39 -0600

    Requerimiento 3: Agregar checkboxes para seleccionar tareas

--- src/app/tarea.ts
+++ src/app/tarea.ts
@@ -3,5 +3,6 @@ export class Tarea {
         public id: number,
         public titulo: string,
         public minutos: number,
+       public seleccionar: boolean = false //propiedad para poder seleccionar
     ){}
 }
