68c4be245ad02414f3b0bfc63ae79ba385dec7ff
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2025-03-04 13:57:34 -0600

    Requerimiento 2: Agregar formulario y método para añadir nuevas tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import { Tarea } from './tarea';
 })
 export class AppComponent {
 	tareas: Tarea[];
+  nuevaTarea: Tarea = new Tarea(0, '', 0);
+
 
 	constructor(
         public service: AppService,
 	) { }
-	
+
 	ngOnInit() {
 		this.obtenerTareas();
 	}
@@ -21,4 +23,32 @@ export class AppComponent {
 	async obtenerTareas() {
 		this.tareas = await this.service.obtenerTareas();
 	}
+
+//creacion de metodo para agregar las tareas
+	agregarTarea() {
+    if(!this.nuevaTarea.titulo || this.nuevaTarea.minutos <=0) {
+      alert('debe ingresar un titulo y duracion')
+      return;
+
+    }
+
+    const nueva = new Tarea(
+			this.tareas.length + 1,
+			this.nuevaTarea.titulo,
+			this.nuevaTarea.minutos
+		);
+
+		// Agregarla al array de tareas
+		this.tareas.push(nueva);
+
+		// Limpiar el formulario
+		this.nuevaTarea = new Tarea(0, '', 0);
+
+		// (Opcional) Log para depuración
+		console.log('Tarea agregada:', nueva);
+		console.log('Tareas actualizadas:', this.tareas);
+	}
 }
+
+
+
